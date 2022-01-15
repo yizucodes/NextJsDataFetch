4c920dd6f1c96f9a93657f7d45a055202acea419
YZu <yizucodes@gmail.com>
2022-01-15 19:55:55 +0100

    Saving API data into context and rendering id

--- data-fetch-compare/pages/_app.js
+++ data-fetch-compare/pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+// import { ApiProvider } from "./apiContext";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ApiProvider>
+//       <Component {...pageProps} />
+//     </ApiProvider>
+//   );
+// }
+
+// export default MyApp;
+
+import React, { useState, useEffect } from "react";
+
+export const ApiContext = React.createContext();
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/1")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return (
+    <ApiContext.Provider value={data}>
+      <ApiData />
+    </ApiContext.Provider>
+  );
 }
 
-export default MyApp
+function ApiData() {
+  const value = React.useContext(ApiContext);
+
+  return <h1>{value?.id}</h1>;
+}
